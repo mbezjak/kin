85d9da3e561b848d88a1bf22212f139844f63138
Miro Bezjak <bezjak.miro@gmail.com>
2011-07-30 20:58:15 +0200

    CommandLineOperations provides common operations on command line.

---
+++ src/main/groovy/hr/helix/kin/cmd/CommandLineOperations.groovy
@@ -0,0 +1,35 @@
+package hr.helix.kin.cmd
+
+/**
+ * Common operations on command line.
+ * 
+ * @author Miro Bezjak
+ * @since 1.0
+ */
+class CommandLineOperations {
+
+    static final int EXIT_CODE_HELP    = 1
+    static final int EXIT_CODE_VERSION = 2
+    static final int EXIT_CODE_NO_BUILD_FILE = 3
+
+    void printHelpAndExit() {
+        error Help.helpInfo, EXIT_CODE_HELP
+    }
+
+    void printVersionAndExit() {
+        error Help.versionInfo, EXIT_CODE_VERSION
+    }
+
+    /**
+     * @param name build file name
+     */
+    void printNoBuildFileAndExit(String name) {
+        error "$name doesn't exist of is not a file", EXIT_CODE_NO_BUILD_FILE
+    }
+
+    void error(String message, int exitCode) {
+        System.err.println message
+        System.exit exitCode
+    }
+
+}

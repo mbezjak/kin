ff0135910adf3ecfcc4c93b02122c5f147d29984
Miro Bezjak <bezjak.miro@gmail.com>
2011-07-30 19:13:38 +0200

    BuildModelScript allows writing DSL code.

---
+++ src/main/groovy/hr/helix/kin/BuildModelScript.groovy
@@ -0,0 +1,24 @@
+package hr.helix.kin
+
+/**
+ * Script allows writing DSL code.
+ * 
+ * @author Miro Bezjak
+ * @since 1.0
+ */
+abstract class BuildModelScript extends Script {
+
+    // prefixed with underscore to avoid name collision
+    final _model = new BuildModel()
+
+    def methodMissing(String name, args) {
+        if (args.size() == 1 && args[0] instanceof Closure) {
+            def job = new Job(name)
+            job.with args[0]
+            _model.add job
+        } else {
+            throw new MissingMethodException(name, delegate, args)
+        }
+    }
+
+}

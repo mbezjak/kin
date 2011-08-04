d7829fd38ebb69a76c26aa4cd19fca2b3a37f047
Miro Bezjak <bezjak.miro@gmail.com>
2011-08-05 01:35:08 +0200

    Quick and dirty implementation of Main.groovy.

---
+++ src/main/groovy/hr/helix/kin/Main.groovy
@@ -0,0 +1,26 @@
+package hr.helix.kin
+
+def terminal = new Terminal()
+new ArgumentsProcessor(terminal).process(args)
+
+def dsl = new IOOperations(terminal).buildFileText
+def model = new ScriptRunner().run(dsl)
+println model
+
+def file = {
+    new File(it).isFile() ? new File(it) : null
+}
+
+def engine = new groovy.text.SimpleTemplateEngine()
+model.producers().each { job ->
+    println model.templates(job.name)
+    def template = model.templates(job.name).findResult file
+    println "template = $template"
+    def traits = model.traits(job.name)
+    println "traits = $traits"
+
+    def config = engine.createTemplate(template).make(traits)
+    def writer = new File("${job.name}.xml").newWriter('utf-8')
+    config.writeTo writer
+    writer.close()
+}
